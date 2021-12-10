bf2daf86d3ebf8059c38bb7a87c80b6acab3ef7e
Caishangqi <2470159730@qq.com>
2021-12-10 02:54:46 -0800

    Initial commit

---
+++ src/main/java/com/harmony.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class harmony extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
