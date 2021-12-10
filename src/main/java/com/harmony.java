8296408577b24d38e54dd51cc337f9245264a3b8
Caishangqi <2470159730@qq.com>
2021-12-10 03:33:48 -0800

    修改启动

--- src/main/java/com/harmony.java
+++ src/main/java/com/harmony.java
@@ -1,5 +1,6 @@
 package com;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class harmony extends JavaPlugin {
@@ -7,7 +8,9 @@ public final class harmony extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println(ChatColor.translateAlternateColorCodes('&', "&7&l[&2+&7] &f加载成功"));
+        //注册默认Config,没有的话创建一个
+        saveDefaultConfig();
     }
 
     @Override
